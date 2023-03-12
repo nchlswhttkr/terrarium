eb83913ef03d7a461ee2062131acd19ef45552e0
Nicholas Whittaker <nicholas@nicholas.cloud>
2023-03-12 14:18:50 +1100

    Build custom Terraform release in CI

--- .buildkite/deploy.sh
+++ .buildkite/deploy.sh
@@ -2,5 +2,9 @@
 
 set -euo pipefail
 
+buildkite-agent artifact download terraform/terraform .
+chmod +x terraform/terraform
+mv terraform/terraform infrastructure/
+
 cd infrastructure
-terraform init
+./terraform init
